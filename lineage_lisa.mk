db30926aa59ac44b34a142a9c5d9b99fd8655d94
Isaiah <lqlp0011@gmail.com>
2024-07-04 13:44:06 +0800

    We're gonna eat some rice with this one...

---
+++ lineage_lisa.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2022 PixelOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lisa device
+$(call inherit-product, device/xiaomi/lisa/device.mk)
+
+# Inherit some common risingOSS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+
+
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+USE_PIXEL_CHARGER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := lisa
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := lineage_lisa
+PRODUCT_MODEL := Xiaomi 11 Lite NE
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="Snapdragon 778G" \
+    RISING_MAINTAINER="Joaquin & Leonardo"
+
+PRODUCT_PACKAGES:= \
+    Photos \
+    MarkupGoogle \
+    LatinIMEGooglePrebuilt \
+    AiWallpapers \
+    WallpaperEmojiPrebuilt \
+    PrebuiltDeskClockGoogle \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt \
+    Velvet
