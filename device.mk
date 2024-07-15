40e684ff0d3dae13bb0c57681b2ea62abb5b4e7c
Isaiah <lqlp0011@gmail.com>
2024-07-14 21:46:43 -0700

    lisa: overlay-awaken: Add maintainer string

--- device.mk
+++ device.mk
@@ -399,6 +399,9 @@ PRODUCT_PACKAGES += \
     LisaTelephonyOverlay \
     LisaWifiOverlay
 
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
